b7446bd59c29ab4089e0f046cb439084fa944f5c
yduanqiaocanxueD <837478687@qq.com>
2018-10-01 18:40:39 +0800

    modify compute_euclidean_distance

--- bin/run.py
+++ bin/run.py
@@ -3,6 +3,7 @@ from similarity_computation.idc_DTW import dtw
 from util.drawer import Drawer
 
 
+# 计算相似度并且画出轨迹
 def test_1():
     index_list = [0, 6, 7, 8, 9, 10]
     reader = FileReader()
@@ -24,5 +25,24 @@ def test_1():
     m_drawer.draw_user_and_reco()
 
 
+def test_2():
+    index_list = range(1100, 1150)
+    reader = FileReader()
+    trajectories = reader.get_some_trajectory(index_list=index_list)
+
+    m_drawer = Drawer()
+    m_drawer.set_user_demand(
+        trajectories[0]["lat"].values, trajectories[0]["lon"].values
+    )
+
+    for i in range(1, len(index_list)):
+        m_drawer.add_reco(
+            trajectories[i]["lat"].values,
+            trajectories[i]["lon"].values,
+            label=str(index_list[i]),
+        )
+    m_drawer.draw_user_and_reco()
+
+
 if __name__ == "__main__":
-    test_1()
+    test_2()
--- similarity_computation/compute_distance.py
+++ similarity_computation/compute_distance.py
@@ -1,6 +1,24 @@
 # coding=utf-8
 import numpy as np
+from math import radians, cos, sin, asin, sqrt
 
-# 计算欧氏距离
-def compute_euclidean_distance(x_1, y_1, x_2, y_2):
-    return np.sqrt(pow(x_1 - x_2, 2) + pow(y_1 - y_2, 2))
+
+# 根据经纬度计算欧氏距离，单位为米
+# 经度1，维度1，经度2，维度2
+def compute_euclidean_distance(lon1, lat1, lon2, lat2):
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # 地球平均半径，单位为公里
+    return c * r * 1000
+
+
+def test():
+    lon1, lat1 = 123.433033, 41.661604  # 东北大学
+    lon2, lat2 = 116.403955, 39.915121  # 天安门
+
+    distance = compute_euclidean_distance(lon1, lat1, lon2, lat2)
+    print("distance=", distance)
